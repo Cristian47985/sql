39150237a089c1885fc25e37b242e4f904b2d316
Cristian47985 <113382239+Cristian47985@users.noreply.github.com>
2023-10-30 10:25:06 +0100

    Add files via upload

---
+++ correctorOrtografico.js
@@ -0,0 +1,99 @@
+var campos
+$(document).ready(function () {
+    campos = document.querySelectorAll(".validar");
+    console.log(campos.length)
+    $("#botonVerificar").click(async function () {
+        campos.forEach(async function (campo) {
+            obtenerTexto()
+            
+            var texto = campo.text().trim();
+            var posiblesReemplazosTabla = [];
+        
+            try {
+                
+                var data = await hacerSolicitudAjax(texto);
+                
+                var matches = data.matches;
+        
+                for (let i = 0; i < matches.length; i++) {
+                    
+                var context = matches[i].context;
+                var palabraErronea = context.text.substring(context.offset, context.offset + context.length);
+                var posiblesReemplazos = [palabraErronea];
+        
+                for (var j = 0; j < matches[i].replacements.length; j++) {
+                    var replacement = matches[i].replacements[j];
+                    posiblesReemplazos.push(replacement.value);
+                    
+                }
+        
+                posiblesReemplazosTabla.push(posiblesReemplazos);
+                }
+        
+                // Llamar a eliminarDuplicados dentro de esta función
+                posiblesReemplazosTabla = eliminarDuplicados(posiblesReemplazosTabla);
+                imprimeCorrecciones(texto, posiblesReemplazosTabla, campo);
+            } catch (error) {
+                console.error("Error en la solicitud AJAX: " + error);
+            }
+            });
+    })
+  });
+  
+  function hacerSolicitudAjax(texto) {
+    return new Promise(function (resolve, reject) {
+      $.get("https://api.languagetool.org/v2/check", { text: texto, language: "es" })
+        .done(function (data) {
+          resolve(data);
+        })
+        .fail(function (error) {
+          reject(error);
+        });
+    });
+  }
+  
+  
+    function imprimeCorrecciones(texto, tabla, campo) {
+      for (let i = 0; i < tabla.length; i++) {
+        const opciones = tabla[i];
+        var selectHTML = "<select style='border-bottom: 3px solid #ff9999; border-radius: 5px;'>";
+        for (var j = 0; j < opciones.length; j++) {
+          selectHTML += "<option value='" + opciones[j] + "'>" + opciones[j] + "</option>";
+        }
+        selectHTML += "</select>";
+        texto = replaceAll(texto, tabla[i][0], selectHTML)
+      }
+      campo.html(texto);
+    }
+  
+  
+    function replaceAll(input, find, replace) {
+      return input.split(find).join(replace);
+    }
+  
+    function eliminarDuplicados(arr) {
+      const objetoUnico = {};
+      const resultado = [];
+  
+      for (let i = 0; i < arr.length; i++) {
+        const elementoSerializado = JSON.stringify(arr[i]);
+        if (!objetoUnico[elementoSerializado]) {
+          objetoUnico[elementoSerializado] = true;
+          resultado.push(arr[i]);
+        }
+      }
+  
+      return resultado;
+    }
+  
+    function obtenerTexto() {
+    var $div = $("#campoTexto");
+  
+    var selects = $div.find("select");
+  
+    selects.each(function(index, select) {
+      var valorSeleccionado = $(select).find("option:selected").text();
+      $(select).replaceWith(valorSeleccionado);
+    });
+  
+  }
